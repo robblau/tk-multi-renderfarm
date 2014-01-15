ca62eee86bff7b2bc999bb0b81418dc639f404cd
Max Persson <max@looplab.se>
2014-01-15 16:41:52 +0100

    Cleanup hooks

--- hooks/pre_submit_tk-maya.py
+++ hooks/pre_submit_tk-maya.py
@@ -1,33 +1,34 @@
+import os
+
 import maya.cmds as cmds
 
 import tank
 from tank import Hook
 from tank import TankError
 
+
 class PreSubmitHook(Hook):
-    
     def execute(self, **kwargs):
-                
-        items = []
-        
+        attrs = []
+
         # get the main scene:
         scene_name = cmds.file(query=True, sn=True)
         if not scene_name:
             raise TankError("Please Save your file before Rendering")
-                 
-        items.append({"type": "work_file", "value": scene_name})
-        
+
+        attrs.append({"type": "work_file", "value": scene_name})
+
         #scan scene for starting information
-        items.append({'type':'start','value':cmds.getAttr('defaultRenderGlobals.startFrame')})
-        items.append({'type':'end','value':cmds.getAttr('defaultRenderGlobals.endFrame')})
-        
+        attrs.append({'type': 'start', 'value': cmds.getAttr('defaultRenderGlobals.startFrame')})
+        attrs.append({'type': 'end', 'value': cmds.getAttr('defaultRenderGlobals.endFrame')})
+
         jobname = '.'.join(os.path.basename(scene_name).split('.')[0:-1])
-        items.append({'type':'jobname','value':jobname})
-        
+        attrs.append({'type': 'jobname', 'value': jobname})
+
         #additional data
         #items.append({'type':'Limit','value':'maya_render'})
-        
+
         #saving scene before dialog
         cmds.file(save=True, force=True)
 
-        return items
+        return attrs
--- hooks/pre_submit_tk-nuke.py
+++ hooks/pre_submit_tk-nuke.py
@@ -1,33 +1,34 @@
+import os
+
 import nuke
 
 import tank
 from tank import Hook
 from tank import TankError
 
+
 class PreSubmitHook(Hook):
-    
     def execute(self, **kwargs):
-                
-        items = []
-        
+        attrs = []
+
         # get the main scene:
         scene_name = nuke.root()['name'].value()
         if not scene_name:
             raise TankError("Please Save your file before Rendering")
-                 
-        items.append({"type": "work_file", "value": scene_name})
-        
+
+        attrs.append({"type": "work_file", "value": scene_name})
+
         #scan scene for starting information
-        items.append({'type':'start','value': nuke.root()['first_frame'].value()})
-        items.append({'type':'end','value': nuke.root()['last_frame'].value()})
-        
+        attrs.append({'type': 'start', 'value': nuke.root()['first_frame'].value()})
+        attrs.append({'type': 'end', 'value': nuke.root()['last_frame'].value()})
+
         jobname = '.'.join(os.path.basename(scene_name).split('.')[0:-1])
-        items.append({'type':'jobname','value':jobname})
-        
+        attrs.append({'type': 'jobname', 'value': jobname})
+
         #adding extra attributes
         #items.append({'type':'Limit','value':'nuke_render'})
-        
+
         #saving scene before dialog
         nuke.scriptSave()
 
-        return items
+        return attrs
